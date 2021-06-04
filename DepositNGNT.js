c8a74612d4d7476829ee6e05f1032a51d43384d6
Gbubemi Agbeyegbe <gbubemi.agbeyegbe@cowriesys.com>
2021-06-04 18:27:40 +0100

    Updated Deposit example

--- DepositNGNT.js
+++ DepositNGNT.js
@@ -3,9 +3,11 @@ const Transfer = require('./SEP6');
 let NGNT = {asset:'NGNT', issuer: 'GAWODAROMJ33V5YDFY3NPYTHVYQG7MJXVJ2ND3AOGIHYRWINES6ACCPD'};
 
 var destinationAccountAddress = 'GB3B2EQSBR4A5WFSR6KBDSTSZNCZLKGEIBI7HCM3XZIGAFCEINAK3QUT';
+var amount = 1000;
 var email = 'gdini2003@gmail.com';
+var name = 'Gbubemi Agbeyegbe'
 
-Transfer.deposit(NGNT.asset, destinationAccountAddress,  null, null, email)
+Transfer.deposit(NGNT.asset, destinationAccountAddress, amount, email, name,  null, null)
 .then((deposit) => {
     console.log(JSON.stringify(deposit));
 });
--- SEP6.js
+++ SEP6.js
@@ -4,9 +4,11 @@ const querystring = require('querystring');
 const url = 'https://api.cowrie.exchange/transfer/';
 
 module.exports = {
-    deposit: (asset_code, account, memo_type, memo, email_address) => {
+    deposit: (asset_code, account, amount, email_address, full_name, memo_type, memo) => {
 
-        var query = querystring.stringify({ asset_code: asset_code, account: account, memo_type: memo_type, memo: memo, email_address: email_address});
+        var query = querystring.stringify({ asset_code: asset_code, account: account, amount: amount, 
+                                            email_address: email_address, full_name: full_name,
+                                            memo_type: memo_type, memo: memo});
         var api = url + 'deposit?' + query;
         console.log(api);
 
